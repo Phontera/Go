299f263156feae10e63572bf649c6640d2b616a3
Peter Oto <pduke360@gmail.com>
2020-04-08 22:09:22 +0100

    First Commit

---
+++ converter.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+//Converter represents the Converter
+type Converter struct {
+	feet        feet
+	centimeters Centimeters
+}
+
+type Feet float64
+type Centimeters float64
+
+//CentimetersToFeet converts Centimeters to Feet
+func (cvr Converter);
+CentimeterToFeet Feet {
+	//conversion code
+	return Feet(float64(c)30.48);
+}
+func main () {
+	var cvr = Converter 
+}
